578323f89b0f086e128977cb4b7fb4f2cd1b7ff3
alivit <75130773+alivit@users.noreply.github.com>
2021-01-28 06:20:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -113,7 +113,9 @@ client.on('group-participants-update', async (anu) => {
 			} else if (anu.action == 'remove') {
 				num = anu.participants[0]
 				try {
-
+					
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
 				teks = `@${num.split('@')[0]} SAIU DO GRUPO.`
 				let buff = await getBuffer(ppimg)
